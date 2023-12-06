5014fdcb30f24f5c2aab805b2fdca6a43ad97f1d
JoshDS595 <joshuadasilva595@gmail.com>
2023-12-06 18:37:29 -0500

    ported work to repository

---
+++ script/script.js
@@ -0,0 +1,27 @@
+// add class navbarDark on navbar scroll
+class YearElement extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML=new Date().getFullYear();
+
+    }
+}
+customElements.define("x-fullyear", YearElement);
+
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
